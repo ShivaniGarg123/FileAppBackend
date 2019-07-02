9f7b2dfd3d464a065ebefa244c31fa3371fa779a
shivani <shivani.garg.cs.2016@miet.ac.in>
2019-07-02 06:55:16 -0700

    Second commit

--- index.js
+++ index.js
@@ -3,20 +3,42 @@ var app=express();
 
 var fs=require('fs');
 const readline = require('readline-sync');
-
+//const replaceString = require('replace-string');
 app.get("/read",(req,res)=>{
-    fs.readFile('matter.txt', (err, data) => {
+    var currentName=readline.question("Enter your file's current name");
+    fs.readFile(currentName, (err, data) => {
         if (err) throw err;
         res.send(data.toString('utf-8'));
     });
 });
-app.get("/write",(req,res)=>{
-    var input=readline.question("What is your name?");
-    fs.appendFile('matter.txt','\t'+input,(err)=>{
+app.get("/writeOrUpdate",(req,res)=>{
+    var currentName=readline.question("Enter your file's current name");
+    var input=readline.question("Enter Text....");
+    fs.appendFile(currentName,'\t'+input,(err)=>{
         if(err) throw err;
         res.send("data written successfully")
     })
 });
+app.get("/deleteFile",(req,res)=>{
+    var currentName=readline.question("Enter your file's current name");
+  fs.unlink(currentName,(err)=>{
+      if(err) throw err;
+      res.send("File deleted successfully");
+  })
+});
+app.get("/renameFile",(req,res)=>{
+    var currentName=readline.question("Enter your file's current name");
+    var newName=readline.question("Enter a new name for file...");
+    fs.rename(currentName,newName,(err)=>{
+        if(err) throw err;
+        res.send("File renamed successfully");
+    })
+});
+
+
+
+
+
 
 app.listen(7867,()=>{
     console.log("server running on a port");
